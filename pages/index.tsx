1e301fd1c297f0ede0b493c5d56edb87b3d5b321
Tyler Weeres <tweeres04@gmail.com>
2021-10-23 09:48:17 -0700

    One column

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,69 +28,52 @@ const Home: NextPage = () => {
 						name="name"
 					/>
 				</div>
-				<div className="columns">
-					<div className="column">
-						<div className="field">
-							<label htmlFor="emailInput" className="label">
-								Email
-							</label>
-							<input
-								type="email"
-								className="input"
-								id="emailInput"
-								name="email"
-							/>
-						</div>
-					</div>
-					<div className="column">
-						<div className="field">
-							<label htmlFor="phoneInput" className="label">
-								Phone number
-							</label>
-							<input
-								type="text"
-								className="input"
-								id="phoneInput"
-								name="phone"
-							/>
-						</div>
+				<div className="field">
+					<label htmlFor="emailInput" className="label">
+						Email
+					</label>
+					<input
+						type="email"
+						className="input"
+						id="emailInput"
+						name="email"
+					/>
+				</div>
+				<div className="field">
+					<label htmlFor="phoneInput" className="label">
+						Phone number
+					</label>
+					<input
+						type="text"
+						className="input"
+						id="phoneInput"
+						name="phone"
+					/>
+				</div>
+				<div className="field">
+					<label htmlFor="projectTypeInput" className="label">
+						Project type
+					</label>
+					<div className="select">
+						<select name="projectType" id="projectTypeInput">
+							<option value=""></option>
+							<option value="new_build">New Build</option>
+							<option value="furniture">Furniture</option>
+							<option value="renovation">Renovation</option>
+							<option value="other">Other</option>
+						</select>
 					</div>
 				</div>
-				<div className="columns">
-					<div className="column">
-						<div className="field">
-							<label htmlFor="projectTypeInput" className="label">
-								Project type
-							</label>
-							<div className="select">
-								<select
-									name="projectType"
-									id="projectTypeInput"
-								>
-									<option value=""></option>
-									<option value="new_build">New Build</option>
-									<option value="furniture">Furniture</option>
-									<option value="renovation">
-										Renovation
-									</option>
-									<option value="other">Other</option>
-								</select>
-							</div>
-						</div>
-					</div>
-					<div className="column">
-						<div className="field">
-							<label htmlFor="startDateInput" className="label">
-								Ideal start date
-							</label>
-							<input
-								type="date"
-								className="input"
-								id="startDateInput"
-								name="startDate"
-							/>
-						</div>
-					</div>
+				<div className="field">
+					<label htmlFor="startDateInput" className="label">
+						Ideal start date
+					</label>
+					<input
+						type="date"
+						className="input"
+						id="startDateInput"
+						name="startDate"
+					/>
 				</div>
 				<div className="field">
 					<label htmlFor="messageInput" className="label">
